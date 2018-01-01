08956638b0886cfbb89ad22c1a636896ac173897
Aman Thakur <designedbyaman@gmail.com>
2018-01-01 17:41:09 +0530

    Finished header and navigation bar

--- js/app.js
+++ js/app.js
@@ -2,3 +2,30 @@ $('.menu-icon1').click(function(e) {
   e.preventDefault();
   $('.dropdown').slideToggle();
 });
+
+$('.carousel').slick({
+  dots: true,
+  slidesToShow: 1,
+  fade: true,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
